ef9258f0a0ffe22147bfc70bcf8d97cbffc79e61
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-17 16:29:54 -0700

    Add sad path test for empty params

--- spec/requests/api/v1/backgrounds/index_spec.rb
+++ spec/requests/api/v1/backgrounds/index_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe 'backgrounds' do
-  it 'it returns background image data that includes id, type, and attributes', :vcr do
+  it 'it returns background image data that includes id, type, and attributes (happy path)', :vcr do
     location_params = {
-      location: 'denver,co'
+      'location': 'denver,co'
     }
 
     get '/api/v1/backgrounds', params: location_params
 
     image = JSON.parse(response.body, symbolize_names: true)
-  
+
     expect(response).to be_successful
 
     expect(image).to be_a(Hash)
@@ -38,5 +38,25 @@ describe 'backgrounds' do
 
     expect(image[:data][:attributes][:image][:attribution]).to have_key(:message)
     expect(image[:data][:attributes][:image][:attribution][:message]).to be_a(String)
+  end
+
+  it 'should return an error message when bad params are passed (sad path)', :vcr do
+    location_params = {
+      'location': ''
+    }
+
+    get '/api/v1/backgrounds', params: location_params
+
+    response_data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response_data).to be_a(Hash)
+    expect(response_data).to have_key(:stat)
+    expect(response_data[:stat]).to eq("fail")
+
+    expect(response_data).to have_key(:message)
+    expect(response_data[:message]).to eq("Parameterless searches have been disabled. Please use flickr.photos.getRecent instead.")
+
+    expect(response_data).to have_key(:status_code)
+    expect(response_data[:status_code]).to eq(400)
   end
 end
