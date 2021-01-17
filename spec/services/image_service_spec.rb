b4c6db964191175894ce945a2a12ed2863b8de48
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-17 16:05:48 -0700

    Create sad path test for image service

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'ImageService', :vcr do
-  it 'should return parsed response data' do
+RSpec.describe 'ImageService' do
+  it 'should return parsed response data (happy path)', :vcr do
     location_params = {
       'location' => 'denver,co'
     }
 
     response = ImageService.get_image_by_location(location_params)
-    
+
     expect(response).to be_a(Hash)
 
     expect(response).to have_key(:photos)
@@ -25,5 +25,16 @@ RSpec.describe 'ImageService', :vcr do
 
     expect(response[:photos][:photo].first).to_not have_key(:iconfarm)
     expect(response[:photos][:photo].first).to_not have_key(:iconserver)
+  end
+
+  it 'should return an error message when bad params are passed (sad path)', :vcr do
+    location_params = {
+      'location' => ''
+    }
+
+    response_data = ImageService.get_image_by_location(location_params)
+    
+    expect(response_data[:stat]).to eq("fail")
+    expect(response_data[:message]).to eq("Parameterless searches have been disabled. Please use flickr.photos.getRecent instead.")
   end
 end
