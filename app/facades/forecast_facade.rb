8e141ad8bb54d65877640ada9d899375e0eac6fe
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-17 16:58:33 -0700

    Update class method to check for status code of response

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,8 +1,13 @@
 class ForecastFacade
   def self.get_forecast(location)
     location_response = GeocodingService.get_geocode_address(location)
-    lat, lng = location_response[:results].first[:locations].first[:latLng][:lat], location_response[:results].first[:locations].first[:latLng][:lng]
-    forecast_response = WeatherService.get_weather(lat, lng)
-    Forecast.new(forecast_response)
+
+    if location_response[:info][:statuscode].zero?
+      lat, lng = location_response[:results].first[:locations].first[:latLng][:lat], location_response[:results].first[:locations].first[:latLng][:lng]
+      forecast_response = WeatherService.get_weather(lat, lng)
+      return Forecast.new(forecast_response)
+    end
+
+    location_response
   end
 end
