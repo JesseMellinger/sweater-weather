58e595bc828e1fe5124dbf09b06b48e0a29a79e6
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-17 16:00:46 -0700

    Add sad path for bad params

--- spec/services/geocoding_service_spec.rb
+++ spec/services/geocoding_service_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'GeocodingService', :vcr do
-  it 'should return parsed response data' do
+RSpec.describe 'GeocodingService' do
+  it 'should return parsed response data (happy path)', :vcr  do
     location_params = {
       'location' => 'denver,co'
     }
     response = GeocodingService.get_geocode_address(location_params)
-    
+
     expect(response).to be_a(Hash)
 
     expect(response).to have_key(:info)
@@ -25,5 +25,18 @@ RSpec.describe 'GeocodingService', :vcr do
     expect(response[:results].first[:locations].first[:latLng]).to be_a(Hash)
     expect(response[:results].first[:locations].first[:latLng]).to have_key(:lat)
     expect(response[:results].first[:locations].first[:latLng]).to have_key(:lng)
+  end
+
+  it 'should return an error message when bad params are passed (sad path)', :vcr do
+    location_params = {
+      'location' => ''
+    }
+
+    response_data = GeocodingService.get_geocode_address(location_params)
+
+    expect(response_data[:info][:statuscode]).to eq(400)
+    expect(response_data[:info][:messages].first).to eq("Illegal argument from request: Insufficient info for location")
+    expect(response_data[:results].first[:providedLocation][:location]).to eq("")
+    expect(response_data[:results].first[:providedLocation][:locations]).to be_nil
   end
 end
