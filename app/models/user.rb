6ca42923e28ceff814300c75e8cce42060cb99de
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-17 20:49:37 -0700

    Create user model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
+require 'securerandom'
+
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence: true
-  validates_presence_of :password, require: true
+  before_save { email.downcase! }
+  before_validation { self.api_key = SecureRandom.uuid }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates_presence_of :password, require: true, length: { minimum: 6 }
   validates :api_key, uniqueness: true, presence: true
 
   has_secure_password
