a9781f2fa863bfe13e85a5f6f079f686d1406d5a
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-18 11:50:20 -0700

    Update action with refactor

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     payload = JSON.parse(request.body.read, symbolize_names: true)
     user = User.new(payload)
-    if user.save
-      return render json: UserSerializer.new(user), status: :created
-    else
-      return render json: {:status=>400, :message=>user.errors.full_messages}
-    end
+
+    return render json: UserSerializer.new(user), status: :created if user.save
+    return render json: {:status=>400, :message=>user.errors.full_messages}
   end
 end
