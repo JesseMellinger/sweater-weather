0a48be03e2dbb00d76215b5aa5e9f9268bfa1bc1
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-19 11:35:52 -0700

    Update test with attributes instead of keys

--- spec/poros/munchie_spec.rb
+++ spec/poros/munchie_spec.rb
@@ -18,17 +18,13 @@ RSpec.describe Munchie do
 
     expect(munchie.destination_city).to be_a(String)
 
-    expect(munchie.forecast).to be_a(Hash)
-    expect(munchie.forecast).to have_key(:summary)
-    expect(munchie.forecast[:summary]).to be_a(String)
-    expect(munchie.forecast).to have_key(:temperature)
-    expect(munchie.forecast[:temperature]).to be_a(String)
+    expect(munchie.forecast).to be_a(CurrentWeather)
+    expect(munchie.forecast.summary).to be_a(String)
+    expect(munchie.forecast.temperature).to be_a(String)
 
-    expect(munchie.restaurant).to be_a(Hash)
-    expect(munchie.restaurant).to have_key(:name)
-    expect(munchie.restaurant[:name]).to be_a(String)
-    expect(munchie.restaurant).to have_key(:address)
-    expect(munchie.restaurant[:address]).to be_a(String)
+    expect(munchie.restaurant).to be_a(Restaurant)
+    expect(munchie.restaurant.name).to be_a(String)
+    expect(munchie.restaurant.address).to be_a(String)
 
     expect(munchie.travel_time).to be_a(String)
   end
