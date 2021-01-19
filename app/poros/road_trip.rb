ed94ff328ea7fa0fddf1a1dbf2977548d12d1945
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-19 15:58:36 -0700

    Refactor road trip poro to create weather object

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -21,28 +21,13 @@ class RoadTrip
     "%d hours, %d minutes" % [hh, mm]
   end
 
-  def get_weather(forecast_data, eta)
-    weather_at_eta = Hash.new
-    temp, conditions = get_eta_temperature(forecast_data, eta)
-    weather_at_eta[:temperature] = temp
-    weather_at_eta[:conditions] = conditions
-    weather_at_eta
-  end
-
-  def get_eta_temperature(forecast_data, eta)
-    unix_eta = Time.now.to_i + eta
-    hours = forecast_data[:hourly].group_by { |hour| hour[:dt] <=> unix_eta }
-    hour = hours[0] ? hours[0].first : hours[-1].last
-    return hour[:temp], hour[:weather].first[:description]
-  end
-
   def set_travel_time(route_and_forecast_data)
     return format_time(route_and_forecast_data.first[:route][:realTime]) if !route_and_forecast_data.empty?
     return "Impossible Route"
   end
 
   def set_weather_at_eta(route_and_forecast_data)
-    return get_weather(route_and_forecast_data.last, route_and_forecast_data.first[:route][:realTime]) if !route_and_forecast_data.empty?
+    return Weather.new(route_and_forecast_data.last, route_and_forecast_data.first[:route][:realTime]) if !route_and_forecast_data.empty?
     return {}
   end
 end
