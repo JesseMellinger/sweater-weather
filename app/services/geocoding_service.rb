075c430e2c52f4886621eecd9bfdb064c69ee3eb
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-18 18:49:38 -0700

    Create class method to fetch directions

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -1,15 +1,24 @@
 class GeocodingService
   def self.get_geocode_address(location)
-    response = conn.get do |req|
+    response = conn.get '/geocoding/v1/address' do |req|
       req.params['key'] = ENV['MAPQUEST_API_KEY']
       req.params['location'] = location['location']
     end
     parse_response(response)
   end
 
+  def self.get_route(origin, destination)
+    response = conn.get '/directions/v2/route' do |req|
+      req.params['key'] = ENV['MAPQUEST_API_KEY']
+      req.params['from'] = origin
+      req.params['to'] = destination
+    end
+    parse_response(response)
+  end
+
   private
   def self.conn
-    Faraday.new('http://www.mapquestapi.com/geocoding/v1/address')
+    Faraday.new('http://www.mapquestapi.com')
   end
 
   def self.parse_response(response)
