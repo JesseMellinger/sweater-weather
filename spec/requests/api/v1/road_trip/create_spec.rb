69ca78d008c940cb97753aef8885d903f5caebff
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-18 17:56:22 -0700

    Create happy path test for road trip endpoint

---
+++ spec/requests/api/v1/road_trip/create_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+describe 'road trip' do
+  before :each do
+    payload = {
+                "email": "whatever@example.com",
+                "password": "password",
+                "password_confirmation": "password"
+              }
+
+    @user = User.create!(payload)
+  end
+  it 'returns a successful response with type, id, attribute keys (happy path)', :vcr do
+    payload = {
+                "origin": "Denver,CO",
+                "destination": "Pueblo,CO",
+                "api_key": @user.api_key
+              }
+
+    post '/api/v1/road_trip', params: payload.to_json
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    endpoint_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(endpoint_response).to be_a(Hash)
+    expect(endpoint_response).to have_key(:data)
+    expect(endpoint_response[:data]).to be_a(Hash)
+
+    expect(endpoint_response[:data]).to have_key(:id)
+    expect(endpoint_response[:data][:id]).to be_nil
+
+    expect(endpoint_response[:data]).to have_key(:type)
+    expect(endpoint_response[:data][:type]).to be_a(String)
+
+    expect(endpoint_response[:data]).to have_key(:attributes)
+    expect(endpoint_response[:data][:attributes]).to be_a(Hash)
+
+    expect(endpoint_response[:data][:attributes]).to have_key(:start_city)
+    expect(endpoint_response[:data][:attributes][:start_city]).to be_a(String)
+
+    expect(endpoint_response[:data][:attributes]).to have_key(:end_city)
+    expect(endpoint_response[:data][:attributes][:end_city]).to be_a(String)
+
+    expect(endpoint_response[:data][:attributes]).to have_key(:travel_time)
+    expect(endpoint_response[:data][:attributes][:travel_time]).to be_a(String)
+
+    expect(endpoint_response[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(endpoint_response[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+
+    expect(endpoint_response[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(endpoint_response[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
+
+    expect(endpoint_response[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+    expect(endpoint_response[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+  end
+end
