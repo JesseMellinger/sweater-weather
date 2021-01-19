ca87e789ed38ca4bf9295f8882286f4971d878a8
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-18 19:49:56 -0700

    Update facade to pass only origin and destination to road trip for impossible route

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -6,8 +6,8 @@ class RoadTripFacade
       lat, lng = route_response[:route][:locations].last[:latLng][:lat], route_response[:route][:locations].last[:latLng][:lng]
       forecast_response = WeatherService.get_weather(lat, lng)
       return RoadTrip.new(origin, destination, route_response, forecast_response)
+    else
+      RoadTrip.new(origin, destination)
     end
-    
-    route_response
   end
 end
