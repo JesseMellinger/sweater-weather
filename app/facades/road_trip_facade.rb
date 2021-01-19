1e5be73b5e460c89c7a505d5017e7f41daf8862d
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2021-01-18 17:57:30 -0700

    Create road trip facade with class method

---
+++ app/facades/road_trip_facade.rb
@@ -0,0 +1,13 @@
+class RoadTripFacade
+  def self.get_trip(origin, destination)
+    route_response = GeocodingService.get_route(origin, destination)
+
+    if route_response[:info][:statuscode].zero?
+      lat, lng = route_response[:route][:locations].last[:latLng][:lat], route_response[:route][:locations].last[:latLng][:lng]
+      forecast_response = WeatherService.get_weather(lat, lng)
+      return RoadTrip.new(origin, destination, route_response, forecast_response)
+    end
+    
+    route_response
+  end
+end
